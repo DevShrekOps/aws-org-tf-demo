cff1f131cfa26db12edc55dac6033748616adeda
Grant Snodgrass <devshrekops@gmail.com>
2024-04-04 23:08:45 -0400

    mgmt-prod: Use S3 backend for storing TF state

---
+++ accounts/mgmt/prod/backend.tf
@@ -0,0 +1,18 @@
+# See README.md in this repo's root directory for commentary on this file. Since the
+# bucket, table, & role referenced below are all declared in this root module, those
+# resources had to be deployed before adding this backend config. That means the state
+# for those resources was temporarily stored in a local `terraform.tfstate` file, but
+# after adding this backend config and re-initializing Terraform, the local state was
+# migrated to the bucket.
+
+terraform {
+  backend "s3" {
+    bucket         = "devshrekops-demo-tf-state-prod"
+    key            = "mgmt-prod"
+    dynamodb_table = "tf-state-locks-prod"
+    region         = "us-east-1"
+    assume_role = {
+      role_arn = "arn:aws:iam::339712815005:role/tf-state-manager-prod"
+    }
+  }
+}
