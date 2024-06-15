7036a28cf8f4b5497982ba1f58b256adf43dcd52
Grant Snodgrass <devshrekops@gmail.com>
2024-06-15 19:19:50 -0400

    mgmt: Consolidate locals into main.tf

--- accounts/mgmt/dev/main.tf
+++ accounts/mgmt/dev/main.tf
@@ -1,3 +1,35 @@
+## -------------------------------------------------------------------------------------
+## LOCALS
+## -------------------------------------------------------------------------------------
+
+# If this locals block becomes inconveniently long, then it probably makes sense to
+# split it up into one or more separate files, but for now I'd rather have this file be
+# longer than there be more files in the directory.
+locals {
+  account_keys = [
+    "mgmt", # management account
+    "sec",  # security account
+  ]
+
+  sso_users = [
+    {
+      username     = "donkey"
+      display_name = "Donkey"
+      email        = "devshrekops+donkey@gmail.com"
+      first_name   = "Donkey"
+      last_name    = "Unknown"
+    },
+  ]
+
+  sso_org_admins = [
+    "donkey",
+  ]
+}
+
+## -------------------------------------------------------------------------------------
+## MODULES
+## -------------------------------------------------------------------------------------
+
 # This module declares a baseline set of resources that's created in every AWS account
 # in this demo.
 module "account_baseline" {
