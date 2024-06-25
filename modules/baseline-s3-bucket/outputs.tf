47de15284e0c9194ad110d84186ef7c110bce6f5
Grant Snodgrass <devshrekops@gmail.com>
2024-06-24 22:41:15 -0400

    baseline-s3-bucket: Make outputs depend on policy

--- modules/baseline-s3-bucket/outputs.tf
+++ modules/baseline-s3-bucket/outputs.tf
@@ -1,9 +1,15 @@
 output "bucket_id" {
   description = "Name of this bucket."
   value       = aws_s3_bucket.main.id
+
+  # Ensure calling modules wait for the bucket's policy to be set before using the
+  # bucket so that resource declarations that depend on the bucket policy don't fail.
+  depends_on = [aws_s3_bucket_policy.main]
 }
 
 output "bucket_arn" {
   description = "ARN of this bucket."
   value       = aws_s3_bucket.main.arn
+
+  depends_on = [aws_s3_bucket_policy.main] # See comment in bucket_id output
 }
